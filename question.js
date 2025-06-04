0663b9cd40cb302993ece166229bb4c3a412de1e
thalia mendes <163300504+thaliamendes@users.noreply.github.com>
2025-06-04 11:01:50 -0300

    Add files via upload

---
+++ question.js
@@ -0,0 +1,36 @@
+export default[
+    {
+        question: "Termine a fraze Maze_______ ?",
+        answers:[
+        { option: "Runner", correct:true },
+        { option: "Run", correct:false },
+        { option: "corra", correct:false},
+        { option: "corrdor", correct:f},
+      ],
+    },
+    {
+        question:"Qunatos filmes maze runner tem ?",
+        answers: [
+            {option: "5", correct:false },
+            {option: "4", correct:true },
+            {option: "3", correct:false },
+        ],
+    },
+    {
+        question:"Quem é o personagem principal ?",
+        answers: [
+            {option: "Thomas", correct:true },
+            {option: "Gregore", correct:false },
+            {option: "Machiel", correct:false },
+        ],
+    },
+    {
+        question: "Qual o nome do virus que destroi o mundo no filme ?",
+        answers: [
+            {option: "The Flare ", correct:true },
+            {option: "flare", correct:false },
+            {option: "o fungo", correct:false },
+        ]
+    }
+]
+
